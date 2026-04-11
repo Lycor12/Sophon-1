edaea3c567e6e6dd9153acca6bb7721aeac081be
Lycor12 <lycors45@gmail.com>
2026-04-11 20:12:02 +0530

    Remove ignored files from tracking

--- target/debug/deps/sophon_config-e19f3f23641f1def.d
+++
@@ -1,7 +0,0 @@
-C:\Users\aniru\Downloads\sophon\target\debug\deps\sophon_config-e19f3f23641f1def.d: crates\sophon-config\src\lib.rs
-
-C:\Users\aniru\Downloads\sophon\target\debug\deps\libsophon_config-e19f3f23641f1def.rlib: crates\sophon-config\src\lib.rs
-
-C:\Users\aniru\Downloads\sophon\target\debug\deps\libsophon_config-e19f3f23641f1def.rmeta: crates\sophon-config\src\lib.rs
-
-crates\sophon-config\src\lib.rs:
--- target/debug/deps/sophon_loss-beb6001a635d5c85.d
+++
@@ -1,9 +0,0 @@
-C:\Users\aniru\Downloads\sophon\target\debug\deps\sophon_loss-beb6001a635d5c85.d: crates\sophon-loss\src\lib.rs crates\sophon-loss\src\ce.rs crates\sophon-loss\src\free_energy.rs
-
-C:\Users\aniru\Downloads\sophon\target\debug\deps\libsophon_loss-beb6001a635d5c85.rlib: crates\sophon-loss\src\lib.rs crates\sophon-loss\src\ce.rs crates\sophon-loss\src\free_energy.rs
-
-C:\Users\aniru\Downloads\sophon\target\debug\deps\libsophon_loss-beb6001a635d5c85.rmeta: crates\sophon-loss\src\lib.rs crates\sophon-loss\src\ce.rs crates\sophon-loss\src\free_energy.rs
-
-crates\sophon-loss\src\lib.rs:
-crates\sophon-loss\src\ce.rs:
-crates\sophon-loss\src\free_energy.rs:
